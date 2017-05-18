794db37d05882f07cdcbc08bfd5dbf88c4a2805c
Chelsea Barocio <chelsea@Chelseas-MacBook-Pro.local>
2017-05-18 13:49:35 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0]
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[3];
+
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped " + cardTwo);
+
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[3]) {
+	alert("you found a match!");
+}	else {
+	alert("sorry, try again.");
+}
+
+
+
+
