a57471ff96b5f3b3aa9782068233ddc81c1280a1
Chelsea Barocio <chelsea@Chelseas-MacBook-Pro.local>
2017-06-14 15:09:24 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 
 var cardsInPlay = [];
 
@@ -12,8 +33,10 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-	console.log("user flipped " + cards[cardId]); 
-	cardsInPlay.push(cards[cardId]);
+	console.log("user flipped " + cards[cardId].rank); 
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay === 2) {
 	checkForMatch();
 	}
