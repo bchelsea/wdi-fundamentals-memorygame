43b9dc2da80a057f618db270da3ad802fe6c6746
Chelsea Barocio <chelsea@Chelseas-MacBook-Pro.local>
2017-06-17 00:35:43 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -21,31 +21,42 @@ var cards = [
 		cardImage: "images/king-of-diamonds.png"
 	}
 ];
-
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	 if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert("You found a match!");
+      cardsInPlay[];
   } else {
-      console.log("Sorry, try again.");
-  }
-}
+      alert("Sorry, try again.");
+      cardsInPlay[];
+  	}
 
-var flipCard = function(cardId) {
+};
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("user flipped " + cards[cardId].rank); 
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay === 2) {
+	if (cardsInPlay.length === 2) {
 	checkForMatch();
 	}
 };
 
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
 
-flipCard(0);
-flipCard(2);
-
+createBoard();
 
 
 
